df37b73fe3569e06ab04551e0db465ffb319942e
fred <fredrickcollins18@gmail.com>
2021-03-15 13:20:42 -0400

    nodejs

---
+++ index.js
@@ -0,0 +1,53 @@
+
+//get required libraries for requests and sqlite3
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const db = require('better-sqlite3')('/home/pi/ice.db')
+
+const requestListener = function (req, res) {
+
+	//handle browser requests for favicon
+	if(req.url === '/favicon.ico') {
+		res.writeHead(404)
+		res.end()
+	
+	//handle request for all car sessions
+	} else if (req.url.indexOf('/api/sessions') === 0) {
+		res.writeHead(200)
+	
+	//select all unique sessions via SQLite3, return as json
+		let rows = db.prepare('SELECT DISTINCT car_session FROM car_data').all()
+		let json = {rows}
+		res.end(JSON.stringify(json))
+	}
+	//handle requests for data
+	else if (req.url.indexOf('/api/data') === 0) {
+		res.writeHead(200)
+	//parse parameters from request url
+		let params = url.parse(req.url, true).query
+	
+	//return error without parameters
+		if(!params.car_session) {
+			res.writeHead(400)
+			res.end()
+		
+		//select all data from the requested session via SQLite3
+		} else {
+			let rows = db.prepare('SELECT * FROM car_data WHERE car_session = ?').all(params.car_session)
+			let json = {rows}
+
+			//return as json
+			res.end(JSON.stringify(json))
+		}
+	
+	//handle requests for others in local filesystem
+	} else {
+		let read = fs.createReadStream("/home/pi" + req.url)
+		read.pipe(res)
+	}
+}
+
+//initialize simple http server on port 8080
+const server = http.createServer(requestListener)
+server.listen(8080)
