b220b2ce0ee0b59b81c594c9a6941b2c013c5985
fredrickcollins <fredrickcollins18@gmail.com>
2021-03-15 14:14:16 -0400

    nodejs

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ const requestListener = function (req, res) {
 	//handle requests for others in local filesystem
 	} else {
 		console.log(req.url)
-		let read = fs.createReadStream(req.url)
+		console.log(__dirname)
+		let read = fs.createReadStream("index.html")
 		read.pipe(res)
 	}
 }
